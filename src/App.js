2dfc63ece7b36fbfb7a43afedbab20c17c231e5c
Lakshya Kumar <kumarlakshya101@gmail.com>
2023-12-19 20:30:43 +0530

    changed the name of the website on tab bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import Home from './pages/Home';
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className='w-full h-screen bg-[#4BC8FE]  px-48 flex items-center justify-center max-xl:px-16 max-lg:px-8 max-sm:px-2'>
-      <Home/>
+    <div className="w-full h-screen bg-[#4BC8FE]  px-48 flex items-center justify-center max-xl:px-16 max-lg:px-8 max-sm:px-2">
+      <Home />
     </div>
   );
 }
