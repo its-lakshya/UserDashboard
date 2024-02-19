48cacec9865f3ddb8772dc07cc8686bfbe8a37af
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-02-19 23:11:27 +0530

    formatted the code

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -3,30 +3,30 @@ export const UserListShimmer = () => {
 
   return (
     <div className="flex item-center justify-between w-full 2xl:w-[70rem] h-[auto] animate-pulse">
-      <div className="flex flex-col gap-y-4 w-[30rem] max-lg:w-[22rem] max-base:w-full">
-        <div className="font-bold text-4xl px-4 h-12"></div>
-        <div className="w-full h-[43rem] overflow-y-scroll flex flex-col justify-start items-center ">
-          {numberOfUsers.map((user, index) => {
-            return (
-              <button
-                className="w-[28rem] my-2 bg-[#eff9ff] flex justify-center items-center rounded-md 
-                                gap-x-12 hover:transition-transform hover:scale-[105%] hover:duration-200 py-2 shadow-lg
-                                shadow-[#696969]"
-                key={index}
-              >
-                <div className="w-16 h-16 rounded-full bg-gray-200"></div>
-                <div className="flex flex-col justify-center items-start w-32 gap-y-3 ">
-                  <span className="font-bold text-lg w-32 bg-gray-200 h-4"></span>
-                  <span className="text-sm w-24 h-4 bg-gray-200"></span>
+          <div className="flex flex-col gap-y-4 w-[30rem] max-lg:w-[22rem] max-base:w-full">
+                <div className="font-bold text-4xl px-4 h-12"></div>
+                <div className="w-full h-[43rem] overflow-y-scroll flex flex-col justify-start items-center ">
+                    {numberOfUsers.map((user, index) => {
+                      return (
+                        <button
+                          className="w-[28rem] my-2 bg-[#eff9ff] flex justify-center items-center rounded-md 
+                                          gap-x-12 hover:transition-transform hover:scale-[105%] hover:duration-200 py-2 shadow-lg
+                                          shadow-[#696969]"
+                          key={index}
+                        >
+                          <div className="w-16 h-16 rounded-full bg-gray-200"></div>
+                          <div className="flex flex-col justify-center items-start w-32 gap-y-3 ">
+                            <span className="font-bold text-lg w-32 bg-gray-200 h-4"></span>
+                            <span className="text-sm w-24 h-4 bg-gray-200"></span>
+                          </div>
+                        </button>
+                      );
+                    })}
                 </div>
-              </button>
-            );
-          })}
-        </div>
-      </div>
-      <div className="w-[25rem] mt-16 max-base:hidden">
-        <UserDetailsShimmer />
-      </div>
+          </div>
+          <div className="w-[25rem] mt-16 max-base:hidden">
+            <UserDetailsShimmer />
+          </div>
     </div>
   );
 };
